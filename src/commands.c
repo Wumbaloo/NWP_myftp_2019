4b20567d3ca6afc73726eaf4acb36e30e83b5a0a
William <william.gaudfrin@epitech.eu>
2020-04-13 15:31:55 +0200

    feat(authentication): get username and password set after USER or PASS command

--- src/commands.c
+++ src/commands.c
@@ -7,33 +7,60 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "ftp.h"
 
-void set_username(int clientfd, void *username)
+void set_username(int clientfd, void *cmd)
 {
-    dprintf(clientfd, "331\n");
-    printf("Client %d has a new username\n", clientfd);
+    char *command = strdup(cmd);
+    char *text = strtok(cmd, " ");
+
+    if (text)
+        text = strtok(NULL, " ");
+    if (!text) {
+        dprintf(clientfd, "430\n");
+        printf("Client %d tried to set a new username\n", clientfd);
+    } else {
+        dprintf(clientfd, "331\n");
+        printf("Client %d has a new username which is %s\n", clientfd, text);
+    }
 }
 
-void set_password(int clientfd, void *password)
+void set_password(int clientfd, void *cmd)
 {
-    dprintf(clientfd, "230\n");
-    printf("Client %d has a new password\n", clientfd);
+    char *command = strdup(cmd);
+    char *text = strtok(cmd, " ");
+
+    if (text)
+        text = strtok(NULL, " ");
+    if (!text) {
+        dprintf(clientfd, "430\n");
+        printf("Client %d tried to set a new password\n", clientfd);
+    } else {
+        dprintf(clientfd, "230\n");
+        printf("Client %d has a new password which is %s\n", clientfd, text);
+    }
 }
 
 void handle_command(int clientfd, char *input, fd_set *active_fd_set)
 {
+    char *base_input = strdup(input);
     char *cmd = strtok(input, " ");
 
     if (!cmd)
         cmd = input;
     for (int i = 0 ; myftp->cmds[i] ; i++) {
         if (strcmp(myftp->cmds[i]->command, cmd) == 0) {
-            myftp->cmds[i]->func(clientfd, active_fd_set);
+            if (strcmp(cmd, "exit") == 0)
+                myftp->cmds[i]->func(clientfd, active_fd_set);
+            else
+                myftp->cmds[i]->func(clientfd, base_input);
+            free(base_input);
             return;
         }
     }
     dprintf(clientfd, "Server: What's next?\n");
+    free(base_input);
 }
 
 void free_commands(void)
